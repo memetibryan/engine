0745bdccc0931f63c1386082b60bfc8b7346fa92
memetibryan <memetibryan@gmail.com>
2022-02-12 14:53:38 +0300

    successfully created the server.js and also created index.ejs in views

--- server.js
+++ server.js
@@ -3,11 +3,27 @@ const app = express()
 
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const bodyparser = require('body-parser')
+const path = require('path')
 
 dotenv.config({path:'config.env'})
 const PORT = process.env.PORT || 8080
 
-app.use(morgan(tiny));
+//log request
+app.use(morgan('tiny'));
+
+//parse request to body parser
+app.use(bodyparser.urlencoded({extended: true}))
+
+//set view enigne
+app.set("view engine","ejs")
+app.set("views", path.resolve(__dirname,"views/ejs"))
+
+//load assets
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
 
 app.get('/', (req, res) => res.send('Engine Application'))
+
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
