0765d15daa9d7587b3da5d302dcbdfe4d9e0c245
memetibryan <memetibryan@gmail.com>
2022-02-12 16:43:35 +0300

    added services functionality for routing

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const route = express.Router()
+const services = require('../services/render');
 
 //routing
-route.get('/',(req,res) =>{
-  res.render('index');
-})
-route.get('/user',(req,res) =>{
-  res.render('user');
-})
-route.get('/hospital',(req,res) =>{
-  res.render('hospital');
-})
+route.get('/',services.homeRoutes);
+
+route.get('/user',services.user);
+
+route.get('/hospital',services.hospital);
 
 module.exports = route
---
+++ server/services/render.js
@@ -0,0 +1,11 @@
+exports.homeRoutes = (req,res)=>{
+  res.render('index');
+}
+
+exports.user = (req,res) =>{
+  res.render('user');
+}
+
+exports.hospital = (req,res) =>{
+  res.render('hospital');
+}
